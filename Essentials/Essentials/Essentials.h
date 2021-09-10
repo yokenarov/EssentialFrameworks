5abf94ffec2ba8fa65e46125d7b12303ee5893fa
Jordan Kenarov <yordan.kenarov@a1.bg>
2021-09-10 08:05:53 +0300

    added my first framework

---
+++ Essentials/Essentials/Essentials.h
@@ -0,0 +1,18 @@
+//
+//  Essentials.h
+//  Essentials
+//
+//  Created by Jordan Kenarov on 10.09.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Essentials.
+FOUNDATION_EXPORT double EssentialsVersionNumber;
+
+//! Project version string for Essentials.
+FOUNDATION_EXPORT const unsigned char EssentialsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Essentials/PublicHeader.h>
+
+
